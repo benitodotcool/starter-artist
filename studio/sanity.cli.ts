f4f8b0e4a63d0bf6d779905990d25d3295632abc
noodles4u <louisdesmett@gmail.com>
2023-11-13 11:57:41 +0100

    env var for quicker dev sanity

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'rv92quiy',
-    dataset: 'production'
-  }
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: 'production',
+  },
 })
--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -4,13 +4,13 @@ import {media} from 'sanity-plugin-media'
 import {noteField} from 'sanity-plugin-note-field'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-import { structure } from './desk'
+import {structure} from './desk'
 
 export default defineConfig({
   name: 'default',
-  title: 'starter UR×BN',
+  title: process.env.SANITY_STUDIO_PROJECT_NAME,
 
-  projectId: 'rv92quiy',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: 'production',
 
   plugins: [deskTool({structure}), visionTool(), noteField(), media()],
